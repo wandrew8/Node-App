eeb04d28602a6312b71abf6833be5105b00a0793
Andrew Weiss <wandrew8@gmail.com>
2020-03-23 15:22:47 -0700

    able to post new images and save to database

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,35 +1,62 @@
+//DOM NODE VARIABLES
 const formButton = document.querySelector('.formButton');
 const addTagButton = document.getElementById('addTagButton');
 const tagAnswers = document.querySelector('.tagAnswers');
+const sampleImageHolder = document.querySelector('.imageSampleHolder');
+const form = document.getElementById('addPhotoForm');
+const formModal = document.querySelector(".formModal");
 
-//CLOUDINARY PHOTO UPLOAD
+//OTHER VARIABLES
+let imageToPost;
+
+//CLOUDINARY PHOTO UPLOAD PRESETS
 const myWidget = cloudinary.createUploadWidget({
     cloudName: 'dcokaa0ia', 
     uploadPreset: 'pixelImages'}, (error, result) => { 
       if (!error && result && result.event === "success") { 
         console.log('Done! Here is the image info: ', result.info);
-        
-      }
+        console.log(result.info.thumbnail_url)
+        imageToPost = result.info.url;
+        sampleImageHolder.innerHTML = `<img height="30" width="30" src="${result.info.thumbnail_url}">`;
+      } 
     }
   )
 
+//FETCH REQUESTS TO SERVER
 function getPhotos() {
     fetch('http://localhost:3000/photos')
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        postPhotos(data);
+        showPhotos(data);
     })
     .catch(err => console.log(err))
 }
 
-function postPhotos(data) {
+async function postPhoto(url = '', data = {}) {
+    const response = await fetch(url, {
+      method: 'POST', 
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow', 
+      referrerPolicy: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return await response.json(); // parses JSON response into native JavaScript objects
+  }
+
+//-----   FUNCTIONS   -----//
+function showPhotos(data) {
     const container = document.querySelector('.photoContainer');
     let html;
     data.forEach(photo => {
         html = `
             <div class="photo">
-                <a href="${photo.imageUrl}" alt="${photo.tags[0] ? photo.tags[0] : ''}">
+                <a href="/photos/${photo._id}" alt="${photo.tags[0] ? photo.tags[0] : ''}">
                 <img class="image" width="200" height="200" src="${photo.imageUrl}">
                 <div class="category">
                     <p>${photo.category}</p>
@@ -44,16 +71,40 @@ function postPhotos(data) {
         `
         container.innerHTML += html;
     })
+};
+
+function validateForm(e) {
+    e.preventDefault();
+    const tags = Array.from(document.querySelectorAll('.smallTag')).map(tag => tag.textContent);
+    const author = document.querySelector('#author').value;
+    const category = document.querySelector('#category').value;
+    const photoObject = {
+        category: category,
+        author: author,
+        tags: tags,
+        imageUrl: imageToPost,
+        likes: 0
+    };
+    console.log(photoObject)
+    postPhoto('http://localhost:3000/photos', photoObject)
+    .then(data => console.log(data))
+    tagAnswers.innerHTML = '';
+    sampleImageHolder.innerHTML = '';
+    form.reset();
+    formModal.classList.remove('showModal');
 }
 
-//EVENT LISTENERS
+//-----   EVENT LISTENERS   -----//
+
+//LOADS PHOTOS ON PAGE LOAD
 document.addEventListener('DOMContentLoaded', getPhotos);
 
+//SHOWS FORM MODAL
 formButton.addEventListener('click', function() {
-    const formModal = document.querySelector(".formModal");
     formModal.classList.add('showModal');
 });
 
+//ADDS PHOTO TAGS
 addTagButton.addEventListener('click', function(e) {
     e.preventDefault();
     let value = document.querySelector("#tagInput");
@@ -61,6 +112,7 @@ addTagButton.addEventListener('click', function(e) {
     value.value = ''; 
 });
 
+//REMOVES PHOTO TAGS
 tagAnswers.addEventListener("click", function(e) {
     console.log(e)
     console.log(e.target)
@@ -69,9 +121,11 @@ tagAnswers.addEventListener("click", function(e) {
     }
 });
 
+// OPENS CLOUDINARY WIDGET
 document.getElementById("upload_widget").addEventListener("click", function(e){
     e.preventDefault();
     myWidget.open();
   }, false);
 
-
+// FORM SUBMIT
+form.addEventListener('submit', validateForm);
