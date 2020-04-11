6189e9710ed4cb6b8594fc604c1f6ce16eb95a43
Andrew Weiss <wandrew8@gmail.com>
2020-04-10 18:21:27 -0700

    deployment issues

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ require('dotenv').config()
 const app = express();
 const mongoose = require('mongoose');
 app.use(cors({
-    origin: 'https://quiet-ravine-27369.herokuapp.com/',
+    origin: 'http://localhost:3000',
 }));
 const connect = mongoose.connect(mongoDB, {
     useCreateIndex: true,
@@ -55,13 +55,13 @@ app.use(function(req, res, next) {
   });
   
   // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // app.use(function(err, req, res, next) {
+  //   // set locals, only providing error in development
+  //   res.locals.message = err.message;
+  //   res.locals.error = req.app.get('env') === 'development' ? err : {};
   
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
+  //   // render the error page
+  //   res.status(err.status || 500);
+  //   res.render('error');
+  // });
   
